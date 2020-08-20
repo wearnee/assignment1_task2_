94247eb77293ec7e5aed103904ee37e967be9c36
Emily Wearne <wearnee@deakin.edu.au>
2020-08-20 19:41:15 +1000

    Co-authored-by: a.bonti@deakin.edu.au <a.bonti@deakin.edu.au>

--- public/env.js
+++ public/env.js
@@ -1,18 +1,149 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
+
+//we keep track of the user it is either null or has a name
+// we initialise to null
+
+let username = null;
+let postId= null;
+
+const getJournals=()=>{
+  $.get('/api/journal',(journals)=>{
+    if(journals.length>0){
+      $('#journal').empty()
+      journals.forEach(journal=>{
+        appendJournal(journal)
+      })
+    }
+  })
 }
 
 
-$(document).ready(function(){
-  console.log('Ready')
-  
-  //bind the button
-  $('#testButton').click(testButtonFunction)
+const commentPost=(element)=>{
+  let test=$(element)
+  postId=test.attr('value')
+  //console.log(element.val())
+}
+const newJournal=()=>{
+  let text= $('#journalText').val()
+  if (username==null) {
+    username = 'Guest'
+    console.log('guest')
+  } 
+  console.log(username)
+  let data={
+    text:text,
+    author:username
+  }
+  $.ajax({
+    url: '/api/journal',
+    contentType: 'application/json',
+    data: JSON.stringify(data), // access in body
+    type: 'POST',
+    success: function(result) {
+        console.log(result)
+    }
+});
+}
 
+
+
+const submitPost=()=>{
+  let text= $('#commentText').val()
+  if (username==null) {
+    username = 'Guest'
+    console.log('guest')
+  } 
+console.log(username)
+  let data={
+    _id:postId,
+    text:text,
+    author:username
+  }
+  console.log(data)
+
+  $.ajax({
+    url: '/api/journal',
+    contentType: 'application/json',
+    data: JSON.stringify(data), // access in body
+    type: 'PUT',
+    success: function(result) {
+        console.log(result)
+    }
+});
+
+}
+
+const login = () => {
+  let user = $('#username').val()
+  if (user.length <= 0) {
+    username = 'Guest'
+    console.log('guest')
+  } else {
+    username = user
+  }
+  $('#buttonLogin').hide()
+
+  $('#user').html(username)
+
+}
+
+
+//handle journal
+const appendJournal = (journal) => {
+ // this iterates through all the comments and creates an object containing all of the comments
+  let jComments=$("<div class='col container'><div>");
+  journal.comments.forEach((comment)=>{
+    let thisComment="<div class='col s12 comment'><i class='material-icons left' style='color:white font-size: larger;'>chat_bubble_outline</i>"+comment.text+" by <b>"+comment.author+"</b></div>"
+    jComments.append(thisComment)
+  })
+  let temp=jComments.html()
+  let jString ="<div class='col s12 journalBox'>\
+    <div class='col s8'>"+journal.author+"</div><div class='col s4'>"+journal.date+"</div>\
+    <div class='col s12 journalText'>"+journal.text+"</div>\
+    <div class='col s12 commentsContainer row'>"+temp+"</div>\
+    <div class='col s12 center'><a id='buttonComment' \
+    onclick=commentPost(this) value="+journal._id+" class='waves-effect waves-light btn modal-trigger' href='#modalComments'>Comment</a><div>\
+  </div>";
+  
+
+  let jEntry = $(jString)
+  //jEntry.append('Sto cazzo')
+
+  $('#journal').append(jEntry)
+
+}
+
+
+$(document).ready(function () {
+
+  console.log('Ready')
+
+  setInterval(()=>{
+    getJournals()
+  },5000)
   //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
+  $.get('/test?user_name="Fantastic User"', (result) => {
     console.log(result)
   })
+  // initialise the tabs
+  $('.tabs').tabs();
 
+  //initialise the map 
+  var map = L.map('worldMap').setView([-37.7, 145], 10);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([-37.5, 144.7]).addTo(map)
+    .bindPopup('Bogan Castle')
+  L.marker([-37.8, 145.1]).addTo(map)
+    .bindPopup('VB Fortress')
+
+    
+
+
+
+  $('.modal').modal();
+  getJournals()
 
 })
--- server.js
+++ server.js
@@ -1,16 +1,126 @@
-var express = require("express"),
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+const e = require('express');
+
+express = require("express"),
     app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
+// need to add the body parser so that we can extract the body data 
+app.use(bodyParser.json());
+
+
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
 
+// logic to insert data
+app.post('/api/journal',(req,res)=>{
+  console.log('body',req.body)
+  let post=req.body;
+  insertJournal(post,res)
+})
+
+//logic to get data
+app.get('/api/journal',(req,res)=>{
+  getJournals(res)
+
+})
+
+
+//logic to udpate data
+app.put('/api/journal',(req,res)=>{
+  console.log('body',req.body)
+  let comment =req.body;
+  updateJournal(comment,res)
+
+})
+
+
 app.listen(port);
 console.log("Listening on port ", port);
 
 require("cf-deployment-tracker-client").track();
+
+const uri = "mongodb+srv://SIT775software:sit775@sit775.hd36d.mongodb.net/messageboard?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+//Comment Collection handler
+let journalCollection;
+const openConnection =(message)=>{
+  client.connect((err,db) => {
+  collectionMessage = client.db("messageboard").collection("messages");
+  if(!err){
+  console.log('connected to db; all systems go')
+  }
+})
+}
+
+// insert journal into the db
+// takes a journal entry, add date to it and pushes into the collection
+const insertJournal=(journal,res)=>{
+  // append date to journal object
+  let date=new Date();
+  journal.date=date.getTime(); 
+  journal.comments=[];
+  // insert into collection
+  journalCollection.insert(journal,(err,result)=>{
+    console.log('Journal Inserted',result)
+    res.send({result:200})
+  }) 
+}
+
+
+// retrieve all journals
+ const getJournals=(res)=>{
+   journalCollection.find().toArray(function(err, result) {
+     if (err) throw err;
+          res.send(result)
+   })
+ }
+
+
+
+// retrieves all journals from the database 
+
+// update journals
+// takes a journal id and a comment entry, add the date to the comment entry nd the updates the journal
+const updateJournal=(comment,res)=>{
+  // we need the ID but we don't need to upload it, so i'll store it locally
+  let id=comment._id;
+  //now i will remove the id from the object
+  delete comment._id
+  //and now we add a server side timestamp
+  let date=new Date();
+  comment.date=date.getTime(); 
+  console.log(id)
+  // to update we are going to use the addToSet property of mongoDB, warning, if you add the same item, it will not work 
+  journalCollection.updateOne(
+    { _id:ObjectID(id) },
+    { $addToSet: { comments: comment  } },(err,result)=>{
+      if(err){
+        console.log(err)
+        res.send({result:'Failed Update'})
+      }else{
+        console.log('Comment added')
+        res.send({result:200})
+      }
+    }
+ )
+
+}
+
+let mockJournal={
+  _id:'5f2f8ae401263f4a7895b478',
+  author:'jimbo text',
+  text:'mor text',
+ 
+}
+
+openConnection()
+
